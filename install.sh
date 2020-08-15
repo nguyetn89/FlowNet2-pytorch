fedcf0536dc761e5f9f897aa6fdf5c01ffefb5f0
nguyetn89 <19274634+nguyetn89@users.noreply.github.com>
2020-08-14 20:49:25 -0400

    update sh

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1       # Request GPU "generic resources"
-#SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-0:20
-#SBATCH --output=./log_install_%N-%j.out
-module load python/3.6
-module load gcc/8.3.0 cuda/10.1
-source ~/ENV/bin/activate
 pip install pypng
 
 cd ./networks/correlation_package
--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#SBATCH --gres=gpu:2       # Request GPU "generic resources"
-#SBATCH --cpus-per-task=16  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-03:30
-#SBATCH --output=./log_calc_%N-%j.out
-module load python/3.6
-source ~/ENV/bin/activate
 #./execute.sh UCSDped2 both
 #./execute.sh Avenue both
 #python3 split_video_sets.py --video "/home/nguyetn/projects/def-jeandiro/nguyetn/datasets/Traffic-Belleview/input.avi" --training_frames 300 --test_frames 300
